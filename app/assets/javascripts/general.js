b53e8b4f5ab3d714ca22cf5f283d65f129a8838b
Rahul <rahulpatil2387@gmail.com>
2014-12-19 17:06:56 +0530

    for edit data retrived

--- app/assets/javascripts/general.js
+++ app/assets/javascripts/general.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	
-  $("#pickup_point_time_detail_route_id").on("change",function(event){
+  $(document).on('change',"#pickup_point_time_detail_route_id",function(event){
   	if ($("#pickup_point_time_detail_route_id").val()!=="")
   	{
   		var routeId = $("#pickup_point_time_detail_route_id").val();	
@@ -17,7 +17,8 @@ $(document).ready(function(){
   	return false;
   });
 
-  $("#route_start_time_container").on("change",function(event){
+
+  $(document).on("change","#pickup_point_time_detail_pickup_route_start_time_id",function(event){
         if ($("#pickup_point_time_detail_route_id").val()!=="")
         {
           var routeId = $("#pickup_point_time_detail_route_id").val();  
@@ -26,7 +27,7 @@ $(document).ready(function(){
               url: 'get_location',
               data: 'route_id='+routeId,
               success: function(msg) {
-                 $(".pickup_time_field").show();
+                 // $(".pickup_time_field").show();
                  $(".pickup_time_field").focus();
               }
         });
