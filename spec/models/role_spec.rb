a0b2880599fd7e623f5561165de57c5b45a70b30
Rahul <rahulpatil2387@gmail.com>
2015-01-05 11:03:45 +0530

    added testcases for role model

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Role, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+
+  before(:each) do
+    @user = FactoryGirl.create(:user)
+  end
+
+  it "should not approve incorrect roles" do
+    @user.add_role :moderator
+    # @user.has_role?(:admin).should be_false
+    expect(@user.has_role?(:admin)).to be_falsey
+  end
+
+  it "should approve correct roles" do
+    @user.add_role :moderator
+    # @user.has_role?(:moderator).should be_true
+      expect(@user.has_role?(:moderator)).to be_truthy
+  end
+
 end
